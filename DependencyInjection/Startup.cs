3c353c6d2b2de2491429d757cf28f19cb89b2a89
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2021-05-27 19:52:02 +0200

    Add factories to IoC

--- DependencyInjection/Startup.cs
+++ DependencyInjection/Startup.cs
@@ -12,15 +12,33 @@ namespace DependencyInjection
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
+            /////////
             // Examples, built in services
             builder.Services.AddLogging();
             builder.Services.AddHttpContextAccessor();
 
+            /////////
             // Examples function implementation specific services
             builder.Services.AddTransient<IPersonService, PersonService>();
+
+            /////////
             // Interfaces are not required
             builder.Services.AddScoped<SomePersonProcess>();
             builder.Services.AddSingleton<SomeUtilityClass>();
+
+            /////////
+            // Factories
+            builder.Services.AddScoped(builder =>
+            {
+                // this is invoked when creating an instance
+                return new SomePersonProcess();
+            });
+            
+            builder.Services.AddTransient<IPersonService>(builder =>
+            {
+                // this is invoked when creating an instance
+                return new PersonService();
+            });
         }
     }
 }
